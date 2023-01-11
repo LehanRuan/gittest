6ed1c00e38e21147a6a0f75d991b95ee59292c7c
Ruan <2789051355@qq.com>
2023-01-11 22:17:04 +0800

    2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+
     }
 }
