31641b81ef6eeeccfcc8eb20cda745cc5ba0ec9c
Ruan <2789051355@qq.com>
2023-01-11 22:42:07 +0800

    Revert "hot - 4"
    This reverts commit 918cf33a

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("111");
-        System.out.println("hello hot-fix");
+        System.out.println("hello hot-fix 4");
     }
 }
